91f1bb6e3e2d3a1c381d9c9462f76f5c3e95aef3
Daniel Guerrero <89276946+danielxpecta@users.noreply.github.com>
2022-08-01 23:43:40 -0500

    Changed maps

--- pages/Individual Report.py
+++ pages/Individual Report.py
@@ -11,8 +11,8 @@ st.set_page_config(page_title="Indv Report")
 st.markdown("# Fake visits report")
 st.write(
     """In this page you can see the amount of red and yellow flags that a rep have.
-- A rep get a yellow flag if the visit location doesnt match with the account location by a ratio of more than 820ft and less than 1640m
-- A rep get a red flag if the visit location doesnt match with the account location by a ratio of more than  1640m
+- A rep get a yellow flag if the visit location doesnt match with the account location by a ratio of more than 724ft and less than 2000m
+- A rep get a red flag if the visit location doesnt match with the account location by a ratio of more than  2000m
 """)
 
 # Upload the data
@@ -25,32 +25,36 @@ if rep !='':
 
     # We filter the dataframe to get only the rep data and group by date to know the yellow and red flags of each day
     rep_data = data[data['name']==rep]
-
-    dates_info = rep_data.groupby('offer_date').sum()[['Yellow flag','Red flag']].sort_index().reset_index()
     
-    fig = px.line(        
-        dates_info, #Data Frame
-        x = "offer_date", #Columns from the data frame
-        y = "Yellow flag",
-        title = "Yellow flags"
-    )
-    fig.update_traces(line_color = "blue")
-    st.plotly_chart(fig)
-
-    fig = px.line(        
-        dates_info, #Data Frame
-        x = "offer_date", #Columns from the data frame
-        y = "Red flag",
-        title = "Red flags"
-    )
-    fig.update_traces(line_color = "red")
-    st.plotly_chart(fig)
+    
 
 
     # We include a date filter in the sidebar to see the information of the visits of that day (Table and Map)
     dates=st.sidebar.date_input("Visit range",[],min_value=rep_data['offer_date'].min(),max_value=rep_data['offer_date'].max())
     if len(dates)==2:
 
+        rep_data = rep_data[(rep_data['date'] >= dates[0]) & (rep_data['date'] <= dates[1])]
+        dates_info = rep_data.groupby('offer_date').sum()[['Yellow flag','Red flag']].sort_index().reset_index()
+
+        fig = px.line(        
+            dates_info, #Data Frame
+            x = "offer_date", #Columns from the data frame
+            y = "Red flag",
+            title = "Red flags"
+        )
+        fig.update_traces(line_color = "red")
+        st.plotly_chart(fig)
+
+        fig = px.line(        
+        dates_info, #Data Frame
+        x = "offer_date", #Columns from the data frame
+        y = "Yellow flag",
+        title = "Yellow flags"
+        )
+        fig.update_traces(line_color = "blue")
+        st.plotly_chart(fig)
+
+        
         # We iterate through all dates between dates selected
         firstDay = dates[0]
         lastDay = dates[1]
@@ -60,7 +64,7 @@ if rep !='':
 
             #For each day we create a expander to keep the information of the day
             with st.expander(f"{i}"):
-                temp_data = rep_data[rep_data['offer_date']==i]
+                temp_data = rep_data[rep_data['date']==i].reset_index(drop=True)
                 if len(temp_data)!=0:
 
                     # We get the red and yellow flags of the day
@@ -69,7 +73,12 @@ if rep !='':
                     st.write("### Map of visits vs locations")
 
                     #We create the app and iterate over the accounts visited that day
-                    map1 = folium.Map(location=[temp_data['visit_lat'].values[0],temp_data['visit_lon'].values[0]], zoom_start=10,tiles='Stamen Toner')
+                    latlon = list(temp_data['coords'][0])
+                    mapit = folium.Map( location=latlon[0], zoom_start=6 ,tiles='Stamen Toner')
+
+                    for coord in latlon:
+                        folium.Circle( location=[ coord[0], coord[1] ], color='#43d9de', radius=10 ).add_to( mapit )
+                    
                     for i in range(len(temp_data)):
 
                         # The account location dot is added in blue
@@ -77,25 +86,55 @@ if rep !='':
                         html = f"{prac_name}'s Location"
                         iframe = folium.IFrame(html)
                         popup = folium.Popup(iframe,min_width=180,max_width=190)
-                        folium.Circle(location=[temp_data.iloc[i]['visit_lat'], temp_data.iloc[i]['visit_lon']],popup=popup,radius=10,fill=True,color='blue',fill_opacity=1).add_to(map1)
 
-                        # The visit location is added in red
-                        html1 = f"{prac_name}'s Visit"
-                        iframe1 = folium.IFrame(html1)
-                        popup1 = folium.Popup(iframe1,min_width=180,max_width=190)
-                        folium.Circle(location=[temp_data.iloc[i]['acc_lat'], temp_data.iloc[i]['acc_lon']],popup=popup1,radius=10,fill=True,color='red',fill_opacity=1).add_to(map1)
-                    folium_static(map1)
+                        red_flag = temp_data.iloc[i]['Red flag']
+                        yellow_flag = temp_data.iloc[i]['Yellow flag']
+                        if red_flag ==1:
+                            color='red'
+                        elif yellow_flag==1:
+                            color='orange'
+                        else:
+                            color='blue'
+                        folium.Circle(location=[temp_data.iloc[i]['acc_lat'], temp_data.iloc[i]['acc_lon']],popup=popup,radius=10,fill=True,color=color,fill_opacity=1).add_to(mapit)
+
+                    mapit.fit_bounds(mapit.get_bounds())
+                    folium_static(mapit)
 
 
                     # We show the table with the basic info of the visits
-                    st.write("### Visits data")           
-                    st.write(temp_data[['practice_name','state','city','status','total_offer','distance_accvisit']].rename(columns={
+                    st.write("### Visits data")        
+                    temp_data =temp_data[['practice_name','state','city','status','total_offer','acc_route_dist']].rename(columns={
                         'practice_name':'Practice Name',
                         'state':'State',
                         'city':'City',
                         'status':'Status',
-                        'total_offer':'Total offer',
-                        'distance_accvisit':'Distance from location'
-                    }))
+                        'total_offer':'Offer',
+                        'acc_route_dist':'Distance'
+                    })   
+                    temp_data['Offer']=temp_data['Offer'].astype(int).astype(str)+' USD'
+                    temp_data['Distance']=temp_data['Distance'].astype(int).astype(str)+' ft     '
+                    st.dataframe(temp_data)
                 else:
                     st.warning("There are no visits this day")
+    else:
+        dates_info = rep_data.groupby('offer_date').sum()[['Yellow flag','Red flag']].sort_index().reset_index()
+
+        fig = px.line(        
+            dates_info, #Data Frame
+            x = "offer_date", #Columns from the data frame
+            y = "Red flag",
+            title = "Red flags"
+        )
+        fig.update_traces(line_color = "red")
+        st.plotly_chart(fig)
+
+        fig = px.line(        
+        dates_info, #Data Frame
+        x = "offer_date", #Columns from the data frame
+        y = "Yellow flag",
+        title = "Yellow flags"
+        )
+        fig.update_traces(line_color = "blue")
+        st.plotly_chart(fig)
+
+        
